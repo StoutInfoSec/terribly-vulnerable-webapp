072e51cff5bc1d7d9d256b190a8be64f0e658aa2
aanton660 <aanton660@gmail.com>
2018-05-01 19:14:43 -0500

    moved footer and header to their own files

--- index.php
+++ index.php
@@ -11,48 +11,7 @@
     <title>Terribly Vulnerable Webapp</title>
 </head>
 <body>
-    <!--**** ~ ~ ~ Navigation Bar ~ ~ ~ *****-->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <!-- TVA logo here -->
-                <a id="logo" class="navbar-brand" href="index.html">TVA</a>
-            </li>
-            <li class="nav-item">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse toolbar" id="collapsibleNavbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="challenges.html">Challenges</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="exploits.html">Exploits</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="payloads.html">Payloads</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="trophies.html">Trophies</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="resources.html">Resources</a>
-                        </li>
-                    </ul>
-                    <div id="search-bar">
-                        <form class="form-inline" action="/action_page.php">
-                            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                            <button class="btn btn-success" type="submit">Search</button>
-                        </form>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </nav>
+    <?php include 'header.php' ?>
     <!-- ****** ~ ~ ~ Floating Side Bar ~ ~ ~ ****** -->
     <div id="floatingUI" class="btn-group-vertical">
         <button type="button" class="btn btn-primary">Hint</button>
@@ -146,24 +105,5 @@
             </div>
         </div>
     </div>
-    <!-- ****** ~ ~ ~ Footer ~ ~ ~ ****** -->
-    <footer class="col">
-        <nav class="navbar navbar-expand-sm bg-dark">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">Help</a>
-                </li>
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">About</a>
-                </li>
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">Disclaimer</a>
-                </li>
-            </ul>
-        </nav>
-    </footer>
 </body>
 </html>
