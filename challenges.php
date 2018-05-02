9a7a335a8a108522a0e3ba19216c7dbf47502e05
aanton660 <aanton660@gmail.com>
2018-05-01 21:06:52 -0500

    switched everything to php
    switched all files to .php and the links as well

--- challenges.php
+++ challenges.php
@@ -13,53 +13,7 @@
     <title>Terribly Vulnerable Webapp</title>
 </head>
 <body>
-    <!--**** ~ ~ ~ Navigation Bar ~ ~ ~ *****-->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <!-- TVA logo here -->
-                <a id="logo" class="navbar-brand" href="index.html">TVA</a>
-            </li>
-            <li class="nav-item">
-                <button class="navbar-toggler" type="button"
-                        data-toggle="collapse" data-target="#collapsibleNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="challenges.html">Challenges</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="exploits.html">Exploits</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="payloads.html">Payloads</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="trophies.html">Trophies</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="resources.html">Resources</a>
-                        </li>
-                    </ul>
-                    <div id="search-bar">
-                        <form class="form-inline" action="/action_page.php">
-                            <input class="form-control mr-sm-2"
-                                   type="text" placeholder="Search">
-                            <button class="btn btn-success"
-                                    type="submit">
-                                Search
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </nav>
+    <?php include 'header.php' ?>
     <!-- ****** ~ ~ ~ Page Body ~ ~ ~ ****** -->
     <div class="well well-sm">
         <div class="jumbotron" id="page-hdr">
@@ -83,7 +37,7 @@
                             <div class="card-body">
                                 <h4 class="card-title"><small>Challenge Name</small></h4>
                                 <p class="card-text"><small>Short Challenge Description</small></p>
-                                <a href="#" class="btn btn-primary">Attempt Challenge</a>
+                                <a href="/challenges/sql/level_1.php" class="btn btn-primary">Attempt Challenge</a>
                             </div>
                         </div>
                     </div>
@@ -191,24 +145,6 @@
             </div>
         </div>
     </div>
-    <!-- ****** ~ ~ ~ Footer ~ ~ ~ ****** -->
-    <footer class="col">
-        <nav class="navbar navbar-expand-sm bg-dark">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">Help</a>
-                </li>
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">About</a>
-                </li>
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <a href="TODO.html" class="nav-link">Disclaimer</a>
-                </li>
-            </ul>
-        </nav>
-    </footer>
+    <?php include 'footer.php' ?>
 </body>
 </html>
